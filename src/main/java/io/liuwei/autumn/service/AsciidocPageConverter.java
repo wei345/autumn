3d6a26892353edcd566ef761c2664acda09d91b3
liuwei <weiliu345@gmail.com>
2020-07-01 20:04:39 +0800

    AsciidocPageConverter enable toc

--- src/main/java/io/liuwei/autumn/service/AsciidocPageConverter.java
+++ src/main/java/io/liuwei/autumn/service/AsciidocPageConverter.java
@@ -2,6 +2,7 @@ package io.liuwei.autumn.service;
 
 import io.liuwei.autumn.data.Page;
 import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.AttributesBuilder;
 import org.asciidoctor.OptionsBuilder;
 import org.asciidoctor.jruby.internal.JRubyAsciidoctor;
 import org.jsoup.Jsoup;
@@ -16,9 +17,13 @@ public class AsciidocPageConverter implements PageConverter {
     @Override
     public Page.PageHtml convert(String title, String body, String path) {
         Asciidoctor asciidoctor = new JRubyAsciidoctor();
-        String html = asciidoctor.convert(body, OptionsBuilder.options().headerFooter(true));
-        Document document = Jsoup.parse(html);
-        html = document.body().html(); // pretty print HTML
-        return new Page.PageHtml(null, title, html);
+
+        String bodyHtml = asciidoctor.convert(body,
+                OptionsBuilder.options().headerFooter(false)
+                        .attributes(AttributesBuilder.attributes().tableOfContents(true)));
+
+        Document document = Jsoup.parse(bodyHtml);
+        bodyHtml = document.body().html(); // pretty print HTML
+        return new Page.PageHtml(null, null, bodyHtml);
     }
 }
